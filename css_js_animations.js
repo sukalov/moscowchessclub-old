472de3f3ab2c2b261133d365ca2cbdee51f46475
sukalov <matvey10@gmail.com>
2023-01-14 11:24:21 -0700

    anim

---
+++ css_js_animations.js
@@ -0,0 +1,68 @@
+const boxes = [];
+const button = document.getElementById("toggle-button");
+const boxContainer = document.getElementById("box-container");
+const animationType = document.getElementById("type");
+
+// create boxes
+for (let i = 0; i < 1000; i++) {
+  const div = document.createElement("div");
+  div.classList.add("css-animation");
+  div.classList.add("box");
+  boxContainer.appendChild(div);
+  boxes.push(div.style);
+}
+
+let toggleStatus = true;
+let rafId;
+button.addEventListener("click", () => {
+  if (toggleStatus) {
+    animationType.textContent = " requestAnimationFrame";
+    for (const child of boxContainer.children) {
+      child.classList.remove("css-animation");
+    }
+    rafId = window.requestAnimationFrame(animate);
+  } else {
+    window.cancelAnimationFrame(rafId);
+    animationType.textContent = " CSS animation";
+    for (const child of boxContainer.children) {
+      child.classList.add("css-animation");
+    }
+  }
+  toggleStatus = !toggleStatus;
+});
+
+const duration = 6000;
+const translateX = 500;
+const rotate = 360;
+const scale = 1.4 - 0.6;
+let start;
+function animate(time) {
+  if (!start) {
+    start = time;
+    rafId = window.requestAnimationFrame(animate);
+    return;
+  }
+
+  const progress = (time - start) / duration;
+  if (progress < 2) {
+    let x = progress * translateX;
+    let transform;
+    if (progress >= 1) {
+      x = (2 - progress) * translateX;
+      transform = `translateX(${x}px) rotate(${
+        (2 - progress) * rotate
+      }deg) scale(${0.6 + (2 - progress) * scale})`;
+    } else {
+      transform = `translateX(${x}px) rotate(${progress * rotate}deg) scale(${
+        0.6 + progress * scale
+      })`;
+    }
+
+    for (const box of boxes) {
+      box.transform = transform;
+    }
+  } else {
+    start = null;
+  }
+  rafId = window.requestAnimationFrame(animate);
+}
