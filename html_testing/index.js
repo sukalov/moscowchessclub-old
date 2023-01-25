3fbae1f189c24f80530d45776d4d93f705885875
sukalov <matvey10@gmail.com>
2023-01-25 03:00:42 +0300

    popup mesdsage

--- html_testing/index.js
+++ html_testing/index.js
@@ -5,3 +5,7 @@ function increment (e){
     count += 1;
     document.getElementById("count").innerText = count;
 }
+
+function thanks(){
+    window.alert("Thank you! Your responces are not important!");
+}
