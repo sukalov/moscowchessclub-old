d7d5044459aacfd3607fbe5e7d0b8b01c76f0e6f
sukalov <matvey10@gmail.com>
2023-02-27 00:44:49 +0300

    new js function, that makes colors-change smooth

--- animated_ava/script.js
+++ animated_ava/script.js
@@ -3,7 +3,10 @@ var html = document.getElementsByTagName("html")[0];
 // var moon = document.getElementsByClassName('.fa-moon')[0];
 // // var toggle_back = document.getElementById('my-toggle');
 
-html.setAttribute("style", "color-scheme: dark");
+// html.setAttribute("style", "color-scheme: dark");
+// html.setAttribute("style", "transition: color-scheme 3s;");
+    document.documentElement.style.setProperty("--text-color", "#ffffff");
+    document.documentElement.style.setProperty("--bg-color", "#111111");
 
 
 // function that changes html color scheme
@@ -18,11 +21,26 @@ function toggleColorScheme() {
   }
 }
 
+function toggleColorScheme2() {
+  var htmlTag = document.getElementsByTagName("html")[0];
+  if (htmlTag.getAttribute("data-color-mode") === "dark") {
+    htmlTag.setAttribute("data-color-mode", "light");
+    document.documentElement.style.setProperty("--bg-color", "#ffffff");
+    document.documentElement.style.setProperty("--text-color", "#111111");
+  } else {
+    htmlTag.setAttribute("data-color-mode", "dark");
+    document.documentElement.style.setProperty("--bg-color", "#111111");
+    document.documentElement.style.setProperty("--text-color", "#ffffff");
+  }
+  document.documentElement.style.transition = "background-color var(--color-scheme-change) ease-in-out, var(--color-scheme-change) ease-in-out";
+}
+
 // function that affects button css and switches toggle position back and forth
 function toggleButton() {
     var toggle = document.getElementById("my-toggle");
     toggle.classList.toggle("on");
     toggle.classList.toggle("off");
-    toggleColorScheme();
+    toggleColorScheme2();
     // finally we call the first function to actually toggle color scheme
 }
+
