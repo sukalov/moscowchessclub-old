cfa64ccb131f21f2a21e4154c44a1552dab88081
sukalov <matvey10@gmail.com>
2023-03-01 16:33:11 +0300

    favicon

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const ejs=require("ejs");
 const bodyParser=require("body-parser");
 const app= express();
 const fs = require('fs');
+const PORT = process.env.PORT || 5002;
 
 //setting the view engine as EJS. 
 app.set('view engine', 'ejs');
@@ -19,6 +20,6 @@ app.get("/", function(req,res){
   res.render("./index");
 });
 
-app.listen(process.env.PORT || 2000, function(){
-        console.log("working at localhost: 4000");
+app.listen(PORT, function(){
+        console.log(`working at localhost: ${ PORT }`);
 })
