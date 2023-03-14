88434c4f6b550a2749e06323189a2e14064458a0
Anatoly Yatskov <yatskovanatoly@gmail.com>
2023-03-14 07:07:20 +0300

    moved chess library

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ async function example() {
 }
 
 
-const gamesPGN = fs.readFileSync(path.resolve(__dirname, 'public/scripts/data/games/game_collection.pgn'), 'utf8')
+const gamesPGN = fs.readFileSync(path.resolve(__dirname, 'chessgames/game_collection.pgn'), 'utf8')
 
 // console.log(gamesPGN);
 
