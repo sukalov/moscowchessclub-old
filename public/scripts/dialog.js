212cad975e41ed01950b9b60f9f04e9a74fb9188
sukalov <matvey10@gmail.com>
2023-03-16 03:29:59 +0300

    BROKEN

--- public/scripts/dialog.js
+++ public/scripts/dialog.js
@@ -1,60 +1,60 @@
-// import game from './data/test_game.json' assert { type: "json" };
-// import game2 from './data/test_game2.json' assert { type: "json" };
-
-
-var jsonRes
+let jsonRes
 let moves
 let moveIndex
 let result
 let getResult
 let playsWhite
-
+let playerOneTurn
 
 function getNewGame() {
+jsonRes = 0;
+moves = 0;
+moveIndex = 0;
+// result = 0;
+getResult = 0;
+playsWhite = 0;
+// playerOneTurn = undefined;
+
   fetch('./new-game')
     .then(response => response.json())
-    .then(data => {
-      (jsonRes = JSON.parse(data), window.jsonRes = jsonRes)
-      return data
-    })
-    .then(data => moves = jsonRes.moves)
-    .then(data => playsWhite = true)
-    .then(data => result = jsonRes.tags.Result)
-    .then(data => {
-      if (result == '1/2-1/2') {
-        getResult = '½-½'
-      }
-      if (result == '1-0' && playsWhite) {
-        getResult = '1-0'
-      } else if (result == '0-1' && playsWhite) {
-        getResult = '0-1'
-      }
-       else if (result == '1-0' && !playsWhite) {
-        getResult = '1-0' 
-      } else {
-        getResult = '0-1'
-      }
-    })
-    .then(data => console.log(moves))
-    .then(data => console.log(jsonRes))
-    .catch(err => console.log(err));
+    .then(data => {(
+      jsonRes = JSON.parse(data),
+      window.jsonRes = jsonRes,
+      moves = jsonRes.moves,
+      result = jsonRes.tags.Result
+    )});
+    // .then(data => playsWhite = true)
+    // .then(data => {
+      // if (result == '1/2-1/2') {
+      //   getResult = '½-½'
+      // }
+      // if (result == '1-0' && playsWhite) {
+      //   getResult = '1-0'
+      // } else if (result == '0-1' && playsWhite) {
+      //   getResult = '0-1'
+      // }
+      //  else if (result == '1-0' && !playsWhite) {
+      //   getResult = '1-0' 
+      // } else {
+      //   getResult = '0-1'
+      // }
+    // })
 }
 
-function start() {
+
+
   const player1 = document.getElementById('motya');
   const player2 = document.getElementById('vanya');
   const movePlayerOne = document.getElementById('movePlayerOne');
   const movePlayerTwo = document.getElementById('movePlayerTwo');
-  let resultMessage = document.getElementById('result')
-  let playAgain = document.getElementById('playAgain')
-  let newGame = document.getElementById('newGame')
-  document.getElementById('playAgain').addEventListener('click', start);
-  document.getElementById('newGame').addEventListener('click', getNewGame);
-  document.getElementById('newGame').addEventListener('click', start);
+  const resultMessage = document.getElementById('result')
+  const playAgain = document.getElementById('playAgain')
+  const newGame = document.getElementById('newGame')
+  playAgain.addEventListener('click', start);
+  newGame.addEventListener('click', getNewGame);
+  newGame.addEventListener('click', start);
   
   // var playerOneTurn = (Math.random() < 0.5);
-  var playerOneTurn
-
 
   function hideMenu() {
     if (moveIndex < moves.length) {
@@ -64,17 +64,22 @@ function start() {
     }
   }
 
-  var firstClickPlayerOne = (function () {
-    var clicked = false;
-    return function (a) {
+  function start() {
+
+    console.log('====' + moves);
+    console.log('====' + jsonRes);
+    console.log('====' + moveIndex);
+    console.log('====' + playsWhite);
+    
+
+    let clicked = false;
+    function firstClickPlayerOne(a) {
       if (!clicked) {
-        clicked = true;
+        clicked = true; // теперь эта функция не будет срабатывать, пока не начнём всё заново
         playerOneTurn = a;
-        moveIndex = 0;
         playsWhite = true;
       }
     }
-  })();
 
   moveIndex = 0;
   console.log(moveIndex);
@@ -161,8 +166,14 @@ function start() {
   window.getNewGame = getNewGame
   window.jsonRes = jsonRes
   window.start = start
-
 }
 
 getNewGame()
 start()
+
+//1. вынести playsWhite и все ифы из фетча куда-нибудь в конец
+// 2. для плеер 1 и плеер 2 20 строчек кода дублируются. оптимизировать чтобы строчки 
+// не дублировались и просто было одно условие вначале
+// то есть вынести в отедльную функцию дублиркубщиеся строчки
+// и обращаться к ним извне в дух случаях с разным порядком аргументов
+
