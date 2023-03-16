33642e6111a18d012ac679bb6f5e89f1c050127b
sukalov <matvey10@gmail.com>
2023-03-17 01:45:45 +0300

    comment out some dialog's coloring

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -9,10 +9,10 @@ var moveB = document.getElementById('moveB');
 // html.setAttribute("style", "transition: color-scheme 3s;");
     html.style.setProperty("--text-color", "#cccccc");
     html.style.setProperty("--bg-color", "#111111");
-    moveW.style.setProperty("--bg-color", "#dddddd");
-    moveW.style.setProperty("--text-color", "#111111");
-    moveB.style.setProperty("--bg-color", "#dddddd");
-    moveB.style.setProperty("--text-color", "#111111");
+    // moveW.style.setProperty("--bg-color", "#dddddd");
+    // moveW.style.setProperty("--text-color", "#111111");
+    // moveB.style.setProperty("--bg-color", "#dddddd");
+    // moveB.style.setProperty("--text-color", "#111111");
 
 // function that changes html color scheme
 function toggleColorScheme() {
@@ -29,19 +29,19 @@ function toggleColorScheme2() {
     html.style.setProperty("--bg-color", "#dddddd");
     html.style.setProperty("--text-color", "#111111");
 
-    moveW.style.setProperty("--bg-color", "#111111");
-    moveW.style.setProperty("--text-color", "#cccccc");
-    moveB.style.setProperty("--bg-color", "#111111");
-    moveB.style.setProperty("--text-color", "#cccccc");
+    // moveW.style.setProperty("--bg-color", "#111111");
+    // moveW.style.setProperty("--text-color", "#cccccc");
+    // moveB.style.setProperty("--bg-color", "#111111");
+    // moveB.style.setProperty("--text-color", "#cccccc");
   } else {
     html.setAttribute("data-color-mode", "dark");
     html.style.setProperty("--bg-color", "#111111");
     html.style.setProperty("--text-color", "#cccccc");
     
-    moveW.style.setProperty("--bg-color", "#dddddd");
-    moveW.style.setProperty("--text-color", "#111111");
-    moveB.style.setProperty("--bg-color", "#dddddd");
-    moveB.style.setProperty("--text-color", "#111111");
+    // moveW.style.setProperty("--bg-color", "#dddddd");
+    // moveW.style.setProperty("--text-color", "#111111");
+    // moveB.style.setProperty("--bg-color", "#dddddd");
+    // moveB.style.setProperty("--text-color", "#111111");
   }
 }
 
@@ -65,8 +65,8 @@ function touchHandle(el) {
   el.classList.toggle("touched");
 }
 
-  const els = document.getElementsByClassName('person__container');
-  Array.prototype.forEach.call(els, el => {
+const els = document.getElementsByClassName('person__container');
+Array.prototype.forEach.call(els, el => {
     el.addEventListener('touchstart', function() {touchHandle(el)});
     el.addEventListener('touchend', function() {touchHandle(el)});
 });
